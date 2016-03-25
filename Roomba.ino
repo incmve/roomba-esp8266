0491419f8f31797b536e59d960c94d120aed853a
incmve <mvegte@gmail.com>
2016-03-25 09:47:46 +0100

    WIP

---
+++ Roomba.ino
@@ -0,0 +1,556 @@
+// Includes
+#include <Time.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+//#include <EEPROM.h>
+#include <SoftwareSerial.h>
+#include "Base64.h"
+#include <FS.h>
+
+String roombotVersion = "0.3.2";
+String WMode = "1";
+#define roombapin 2
+
+// Div
+File UploadFile;
+String fileName;
+String  BSlocal = "0";
+int FSTotal;
+int FSUsed;
+
+//-------------- FSBrowser application -----------
+//format bytes
+String formatBytes(size_t bytes) {
+  if (bytes < 1024) {
+    return String(bytes) + "B";
+  } else if (bytes < (1024 * 1024)) {
+    return String(bytes / 1024.0) + "KB";
+  } else if (bytes < (1024 * 1024 * 1024)) {
+    return String(bytes / 1024.0 / 1024.0) + "MB";
+  } else {
+    return String(bytes / 1024.0 / 1024.0 / 1024.0) + "GB";
+  }
+}
+
+// WIFI
+String ssid    = "ssid";
+String password = "password";
+String espName    = "Roombot";
+
+// webserver
+ESP8266WebServer  server(80);
+MDNSResponder   mdns;
+WiFiClient client;
+
+// AP mode when WIFI not available
+const char *APssid = "Roombot";
+const char *APpassword = "thereisnospoon";
+
+
+
+// Pimatic settings
+String host   = "192.168.x.x";
+const int httpPort    = 80;
+String Username     = "admin";
+String Password     = "password";
+char authVal[40];
+char authValEncoded[40];
+String ClientIP;
+
+
+#define BASE64_LEN 40
+char unameenc[BASE64_LEN];
+
+
+// HTML
+String header       =  "<html lang='en'><head><title>Roombot control panel</title><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><link rel='stylesheet' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css'><script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script><script src='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js'></script></head><body>";
+String navbar       =  "<nav class='navbar navbar-default'><div class='container-fluid'><div class='navbar-header'><a class='navbar-brand' href='/'>Roombot control panel</a></div><div><ul class='nav navbar-nav'><li><a href='/'><span class='glyphicon glyphicon-question-sign'></span> Status</a></li><li class='dropdown'><a class='dropdown-toggle' data-toggle='dropdown' href='#'><span class='glyphicon glyphicon-cog'></span> Tools<span class='caret'></span></a><ul class='dropdown-menu'><li><a href='/updatefwm'>Firmware</a></li><li><a href='/filemanager.html'>File manager</a><a href='/fupload'>File upload</a><a href='/restart'>restart</a></li></ul></li><li><a href='https://github.com/incmve/roomba-eps8266/wiki' target='_blank'><span class='glyphicon glyphicon-question-sign'></span> Help</a></li></ul></div></div></nav>";
+String containerStart   =  "<div class='container'><div class='row'>";
+String containerEnd     =  "<div class='clearfix visible-lg'></div></div></div>";
+String siteEnd        =  "</body></html>";
+
+String panelHeaderName    =  "<div class='col-md-4'><div class='page-header'><h1>";
+String panelHeaderEnd   =  "</h1></div>";
+String panelEnd       =  "</div>";
+
+String panelBodySymbol    =  "<div class='panel panel-default'><div class='panel-body'><span class='glyphicon glyphicon-";
+String panelBodyName    =  "'></span> ";
+String panelBodyValue   =  "<span class='pull-right'>";
+String panelcenter   =  "<div class='row'><div class='span6' style='text-align:center'>";
+String panelBodyEnd     =  "</span></div></div>";
+
+String inputBodyStart   =  "<form action='' method='POST'><div class='panel panel-default'><div class='panel-body'>";
+String inputBodyName    =  "<div class='form-group'><div class='input-group'><span class='input-group-addon' id='basic-addon1'>";
+String inputBodyPOST    =  "</span><input type='text' name='";
+String inputBodyClose   =  "' class='form-control' aria-describedby='basic-addon1'></div></div>";
+String roombacontrol     =  "<a href='/roombastart'<button type='button' class='btn btn-default'><span class='glyphicon glyphicon-play' aria-hidden='true'></span> Start</button></a><a href='/roombadock'<button type='button' class='btn btn-default'><span class='glyphicon glyphicon-home' aria-hidden='true'></span> Dock</button></a></div>";
+
+
+// ROOT page
+void handle_root()
+{
+
+  // get IP
+  IPAddress ip = WiFi.localIP();
+  ClientIP = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+  delay(500);
+
+  String title1     = panelHeaderName + String("Roombot Settings") + panelHeaderEnd;
+  String IPAddClient    = panelBodySymbol + String("globe") + panelBodyName + String("IP Address") + panelBodyValue + ClientIP + panelBodyEnd;
+  String ClientName   = panelBodySymbol + String("tag") + panelBodyName + String("Client Name") + panelBodyValue + espName + panelBodyEnd;
+  String Version     = panelBodySymbol + String("info-sign") + panelBodyName + String("Roombot Version") + panelBodyValue + roombotVersion + panelBodyEnd;
+  String Uptime     = panelBodySymbol + String("time") + panelBodyName + String("Uptime") + panelBodyValue + hour() + String(" h ") + minute() + String(" min ") + second() + String(" sec") + panelBodyEnd + panelEnd;
+
+
+  String title2     = panelHeaderName + String("Pimatic server") + panelHeaderEnd;
+  String IPAddServ    = panelBodySymbol + String("globe") + panelBodyName + String("IP Address") + panelBodyValue + host + panelBodyEnd;
+  String User     = panelBodySymbol + String("user") + panelBodyName + String("Username") + panelBodyValue + Username + panelBodyEnd + panelEnd;
+
+
+  String title3 = panelHeaderName + String("Commands") + panelHeaderEnd;
+  String commands = panelBodySymbol + panelBodyName + panelcenter + roombacontrol + panelBodyEnd;
+
+
+  server.send ( 200, "text/html", header + navbar + containerStart + title1 + IPAddClient + ClientName + Version + Uptime + title3 + commands + containerEnd + siteEnd);
+}
+
+
+// Setup
+void setup(void)
+{
+  Serial.begin(115200);
+  // Check if SPIFFS is OK
+  if (!SPIFFS.begin())
+  {
+    Serial.println("SPIFFS failed, needs formatting");
+    handleFormat();
+    delay(500);
+    ESP.restart();
+  }
+  else
+  {
+    FSInfo fs_info;
+    if (!SPIFFS.info(fs_info))
+    {
+      Serial.println("fs_info failed");
+    }
+    else
+    {
+      FSTotal = fs_info.totalBytes;
+      FSUsed = fs_info.usedBytes;
+    }
+  }
+  //mySerial.begin(115200);  // uncomment this line to use SoftSerial
+  pinMode(roombapin, OUTPUT); // sets the pins as output
+  digitalWrite(roombapin, LOW);
+  WiFi.begin(ssid.c_str(), password.c_str());
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED && i < 31)
+  {
+    delay(1000);
+    Serial.print(".");
+    ++i;
+  }
+  if (WiFi.status() != WL_CONNECTED && i >= 30)
+  {
+    WiFi.disconnect();
+    delay(1000);
+    Serial.println("");
+    Serial.println("Couldn't connect to network :( ");
+    Serial.println("Setting up access point");
+    Serial.println("SSID: ");
+    Serial.println(APssid);
+    Serial.println("password: ");
+    Serial.println(APpassword);
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(APssid, APpassword);
+    WMode = "AP";
+    Serial.print("Connected to ");
+    Serial.println(APssid);
+    IPAddress myIP = WiFi.softAPIP();
+    Serial.print("IP address: ");
+    Serial.println(myIP);
+
+  }
+  else
+  {
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    Serial.print("Hostname: ");
+    Serial.println(espName);
+
+  }
+
+  server.on ( "/format", handleFormat );
+  server.on("/", handle_root);
+  server.on("/", handle_fupload_html);
+  server.on("/api", handle_api);
+  server.on("/updatefwm", handle_updatefwm_html);
+  server.on("/fupload", handle_fupload_html);
+  server.on("/filemanager_ajax", handle_filemanager_ajax);
+  server.on("/delete", handleFileDelete);
+  server.on("/roombastart", handle_roomba_start);
+  server.on("/roombadock", handle_roomba_dock);
+  server.on("/restart", handle_esp_restart);
+
+
+  // Upload firmware:
+  server.on("/updatefw2", HTTP_POST, []() {
+    server.sendHeader("Connection", "close");
+    server.sendHeader("Access-Control-Allow-Origin", "*");
+    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+    ESP.restart();
+  }, []()
+  {
+    HTTPUpload& upload = server.upload();
+    if (upload.status == UPLOAD_FILE_START)
+    {
+      fileName = upload.filename;
+      Serial.setDebugOutput(true);
+      Serial.printf("Update: %s\n", upload.filename.c_str());
+      uint32_t maxSketchSpace = (ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000;
+      if (!Update.begin(maxSketchSpace)) { //start with max available size
+        Update.printError(Serial);
+      }
+    }
+    else if (upload.status == UPLOAD_FILE_WRITE)
+    {
+      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize)
+      {
+        Update.printError(Serial);
+      }
+    }
+    else if (upload.status == UPLOAD_FILE_END)
+    {
+      if (Update.end(true)) //true to set the size to the current progress
+      {
+        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+      }
+      else
+      {
+        Update.printError(Serial);
+      }
+      Serial.setDebugOutput(false);
+
+    }
+    yield();
+  });
+  // upload file to SPIFFS
+  server.on("/fupload2", HTTP_POST, []() {
+    server.sendHeader("Connection", "close");
+    server.sendHeader("Access-Control-Allow-Origin", "*");
+    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+  }, []() {
+    HTTPUpload& upload = server.upload();
+    if (upload.status == UPLOAD_FILE_START)
+    {
+      fileName = upload.filename;
+      Serial.setDebugOutput(true);
+      //fileName = upload.filename;
+      Serial.println("Upload Name: " + fileName);
+      String path;
+      if (fileName.indexOf(".css") >= 0)
+      {
+        path = "/css/" + fileName;
+      }
+      else if (fileName.indexOf(".js") >= 0)
+      {
+        path = "/js/" + fileName;
+      }
+      else if (fileName.indexOf(".otf") >= 0 || fileName.indexOf(".eot") >= 0 || fileName.indexOf(".svg") >= 0 || fileName.indexOf(".ttf") >= 0 || fileName.indexOf(".woff") >= 0 || fileName.indexOf(".woff2") >= 0)
+      {
+        path = "/fonts/" + fileName;
+      }
+      else
+      {
+        path = "/" + fileName;
+      }
+      UploadFile = SPIFFS.open(path, "w");
+      // already existing file will be overwritten!
+    }
+    else if (upload.status == UPLOAD_FILE_WRITE)
+    {
+      if (UploadFile)
+        UploadFile.write(upload.buf, upload.currentSize);
+      Serial.println(fileName + " size: " + upload.currentSize);
+    }
+    else if (upload.status == UPLOAD_FILE_END)
+    {
+      Serial.print("Upload Size: ");
+      Serial.println(upload.totalSize);  // need 2 commands to work!
+      if (UploadFile)
+        UploadFile.close();
+    }
+    yield();
+  });
+
+  //called when the url is not defined here
+  //use it to load content from SPIFFS
+  server.onNotFound([]() {
+    if (!handleFileRead(server.uri()))
+      server.send(404, "text/plain", "FileNotFound");
+  });
+
+  if (!mdns.begin(espName.c_str(), WiFi.localIP())) {
+    Serial.println("Error setting up MDNS responder!");
+    while (1) {
+      delay(1000);
+    }
+  }
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+String getContentType(String filename) {
+  if (server.hasArg("download")) return "application/octet-stream";
+  else if (filename.endsWith(".htm")) return "text/html";
+  else if (filename.endsWith(".html")) return "text/html";
+  else if (filename.endsWith(".css")) return "text/css";
+  else if (filename.endsWith(".js")) return "application/javascript";
+  else if (filename.endsWith(".png")) return "image/png";
+  else if (filename.endsWith(".gif")) return "image/gif";
+  else if (filename.endsWith(".jpg")) return "image/jpeg";
+  else if (filename.endsWith(".ico")) return "image/x-icon";
+  else if (filename.endsWith(".xml")) return "text/xml";
+  else if (filename.endsWith(".pdf")) return "application/x-pdf";
+  else if (filename.endsWith(".zip")) return "application/x-zip";
+  else if (filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
+}
+
+bool handleFileRead(String path)
+{
+  Serial.println("handleFileRead: " + path);
+  if (path.endsWith("/")) path += "root.html";
+  if (path == "/js/insert.js" && BSlocal != "1")
+  {
+    path = "/js/insert-web.js";
+  }
+  if (path == "/js/insert.js" && BSlocal == "1")
+  {
+    path = "/js/insert-local.js";
+  }
+
+
+  String contentType = getContentType(path);
+  String pathWithGz = path + ".gz";
+  if (SPIFFS.exists(pathWithGz) || SPIFFS.exists(path))
+  {
+    if (SPIFFS.exists(pathWithGz))
+      path += ".gz";
+    File file = SPIFFS.open(path, "r");
+    if ( (path.startsWith("/css/") || path.startsWith("/js/") || path.startsWith("/fonts/")) &&  !path.startsWith("/js/insert"))
+    {
+      server.sendHeader("Cache-Control", " max-age=31104000");
+    }
+    else
+    {
+      server.sendHeader("Connection", "close");
+    }
+    size_t sent = server.streamFile(file, contentType);
+    size_t contentLength = file.size();
+    file.close();
+    return true;
+  }
+  else
+  {
+    //Serial.println(path);
+  }
+  return false;
+}
+
+// LOOP
+void loop(void)
+{
+
+  server.handleClient();
+
+}
+
+
+// handles
+
+void handle_api()
+{
+  // Get vars for all commands
+  String action = server.arg("action");
+  String value = server.arg("value");
+  String api = server.arg("api");
+
+  if (action == "clean" && value == "start")
+  {
+    handle_roomba_start();
+
+  }
+
+  if (action == "dock" && value == "home")
+  {
+    handle_roomba_dock();
+  }
+  if (action == "reset" && value == "true")
+  {
+    server.send ( 200, "text/html", "Reset ESP OK");
+    delay(500);
+    Serial.println("RESET");
+    ESP.restart();
+  }
+}
+
+void handle_updatefwm_html()
+{
+  server.send ( 200, "text/html", "<form method='POST' action='/updatefw2' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Update'></form><br<b>For firmware only!!</b>");
+}
+
+
+void handle_fupload_html()
+{
+  String HTML = "<br>Files on flash:<br>";
+  Dir dir = SPIFFS.openDir("/");
+  while (dir.next())
+  {
+    fileName = dir.fileName();
+    size_t fileSize = dir.fileSize();
+    HTML += fileName.c_str();
+    HTML += " ";
+    HTML += formatBytes(fileSize).c_str();
+    HTML += " , ";
+    HTML += fileSize;
+    HTML += "<br>";
+    //Serial.printf("FS File: %s, size: %s\n", fileName.c_str(), formatBytes(fileSize).c_str());
+  }
+
+  server.send ( 200, "text/html", "<form method='POST' action='/fupload2' enctype='multipart/form-data'><input type='file' name='update' multiple><input type='submit' value='Update'></form><br<b>For webfiles only!!</b>Multiple files possible<br>" + HTML);
+}
+
+
+
+void handle_update_upload()
+{
+  if (server.uri() != "/update2") return;
+  HTTPUpload& upload = server.upload();
+  if (upload.status == UPLOAD_FILE_START) {
+    Serial.setDebugOutput(true);
+    Serial.printf("Update: %s\n", upload.filename.c_str());
+    uint32_t maxSketchSpace = (ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000;
+    if (!Update.begin(maxSketchSpace)) { //start with max available size
+      Update.printError(Serial);
+    }
+  } else if (upload.status == UPLOAD_FILE_WRITE) {
+    if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+      Update.printError(Serial);
+    }
+  } else if (upload.status == UPLOAD_FILE_END) {
+    if (Update.end(true)) { //true to set the size to the current progress
+      Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+    } else {
+      Update.printError(Serial);
+    }
+    Serial.setDebugOutput(false);
+  }
+  yield();
+}
+void handle_update_html2()
+{
+  server.sendHeader("Connection", "close");
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+  server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+  ESP.restart();
+}
+void handleFileDelete()
+{
+  if (server.args() == 0) return server.send(500, "text/plain", "BAD ARGS");
+  String path = server.arg(0);
+  if (!path.startsWith("/")) path = "/" + path;
+  Serial.println("handleFileDelete: " + path);
+  if (path == "/")
+    return server.send(500, "text/plain", "BAD PATH");
+  if (!SPIFFS.exists(path))
+    return server.send(404, "text/plain", "FileNotFound");
+  SPIFFS.remove(path);
+  server.send(200, "text/plain", "");
+  path = String();
+}
+void handleFormat()
+{
+  server.send ( 200, "text/html", "OK");
+  Serial.println("Format SPIFFS");
+  if (SPIFFS.format())
+  {
+    if (!SPIFFS.begin())
+    {
+      Serial.println("Format SPIFFS failed");
+    }
+  }
+  else
+  {
+    Serial.println("Format SPIFFS failed");
+  }
+  if (!SPIFFS.begin())
+  {
+    Serial.println("SPIFFS failed, needs formatting");
+  }
+  else
+  {
+    Serial.println("SPIFFS mounted");
+  }
+}
+void handle_filemanager_ajax()
+{
+  String form = server.arg("form");
+  if (form != "filemanager")
+  {
+    String HTML;
+    Dir dir = SPIFFS.openDir("/");
+    while (dir.next())
+    {
+      fileName = dir.fileName();
+      size_t fileSize = dir.fileSize();
+      HTML += String("<option>") + fileName + String("</option>");
+    }
+
+    // Glue everything together and send to client
+    server.send(200, "text/html", HTML);
+  }
+}
+
+void handle_roomba_start()
+{
+  Serial.println("Starting");
+  // digitalWrite(roombapin, HIGH); wakeup no longer needed newer roomba https://forum.arduino.cc/index.php?topic=168377.msg1254745#msg1254745
+  delay(2000);
+  // digitalWrite(roombapin, LOW);
+  delay(50);
+  Serial.write(128);
+  //delay(50);
+  //Serial.write(129);
+  //Serial.write(11);
+  delay(50);
+  Serial.write(131);
+  delay(50);
+  Serial.write(135);
+  Serial.println("I will clean master");
+
+
+  handle_root();
+}
+void handle_roomba_dock()
+{
+  Serial.write(143);
+  Serial.println("Thank you for letting me rest, going home master");
+  handle_root();
+}
+
+
+void handle_esp_restart() {
+  ESP.restart();
+}
+
+void handle_wifim_html()
+{
+  server.send ( 200, "text/html", "<form method='POST' action='/wifi_ajax'><input type='hidden' name='form' value='wifi'><input type='text' name='ssid'><input type='password' name='password'><input type='submit' value='Submit'></form><br<b>Enter WiFi credentials</b>");
+}
---
+++ SPIFFS upload/insert.js
@@ -0,0 +1,4 @@
+document.write('<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">');
+document.write('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">');
+document.write('<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>');
+document.write('<script type="text/javascript" src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>');
